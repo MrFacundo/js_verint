03805726dcb37bcea914572468954e75507f8035
MrFacundo <ftroitero@gmail.com>
2025-05-28 14:37:58 +0100

    aded funcitons

--- flag5.js
+++ flag5.js
@@ -1,18 +1,22 @@
 // Set the value of a question field based on previous answers, so it can be used as a flag in subsequent questions
 $(function () {
-    const flags = {
-        Q205_1: ["%[13]Q177_A_1%", "%[13]Q177_A_10%"],
-        Q205_2: ["%[13]Q177_A_2%", "%[13]Q177_A_11%"],
-        Q205_3: ["%[13]Q177_A_3%", "%[13]Q177_A_12%"],
-        Q205_4: ["%[13]Q177_A_4%", "%[13]Q177_A_13%"]
-    };
+    const flags = [
+        { field: 'Q210_1', values: ["%[19]Q192_A_25%", "%[19]Q192_A_33%"] },
+        { field: 'Q210_2', values: ["%[19]Q192_A_26%", "%[19]Q192_A_34%"] },
+        { field: 'Q210_3', values: ["%[19]Q192_A_27%", "%[19]Q192_A_35%"] },
+        { field: 'Q210_4', values: ["%[19]Q192_A_28%", "%[19]Q192_A_36%"] },
+        { field: 'Q210_5', values: ["%[19]Q192_A_29%", "%[19]Q192_A_37%"] },
+        { field: 'Q210_6', values: ["%[19]Q192_A_30%", "%[19]Q192_A_38%"] },
+        { field: 'Q210_7', values: ["%[19]Q192_A_31%", "%[19]Q192_A_39%"] },
+        { field: 'Q210_8', values: ["%[19]Q192_A_32%", "%[19]Q192_A_40%"] }
+    ];
+    console.log("flags", flags);
     let anyFlagTrue = false;
-    Object.entries(flags).forEach(([field, values]) => {
-        const isTrue = values.some(value => value == 1) ? 1 : 0;
-        $(`#${field}`).val(isTrue);
-        console.log(`Updated ${field} with value: ${isTrue}`);
-        anyFlagTrue = anyFlagTrue || isTrue;
+    flags.forEach(flag => {
+        const isAnyTrue = flag.values.some(value => value == 1) ? 1 : 0;
+        $(`#${flag.field}`).val(isAnyTrue);
+        anyFlagTrue = anyFlagTrue || isAnyTrue;
     });
-    $('#Q205_5').val(anyFlagTrue ? 1 : 0);
-    $('#BN').trigger('click');
+    $('#Q210_9').val(anyFlagTrue ? 1 : 0);
+    //$('#BN').trigger('click');
 });
--- matrix/shuffleColumnsButLast.js
+++ matrix/shuffleColumnsButLast.js
@@ -1,7 +1,7 @@
-// Shuffle columns of a table (desktop layout), excluding the last column
+// Shuffle columns of a table if it exists, otherwise shuffle li elements inside ol
 $(function () {
-	function shuffleColumns() {
-		const table = $('table');
+	const table = $('table');
+	if (table.length) {
 		const rows = table.find('tr');
 		const columns = rows.first().find('th').slice(1, -1);
 		const columnIndices = [...Array(columns.length).keys()];
@@ -20,7 +20,20 @@ $(function () {
 			const shuffledCells = columnIndices.map(index => middleCells.eq(index));
 			$(this).empty().append(firstCell).append(shuffledCells).append(lastCell);
 		});
-	}
+	} else {
+		$('ol').each(function () {
+			const items = $(this).children('li').not(':first').not(':last');
+			const firstItem = $(this).children('li:first');
+			const lastItem = $(this).children('li:last');
+			const itemIndices = [...Array(items.length).keys()];
 
-	shuffleColumns();
-}); 
+			for (let i = itemIndices.length - 1; i > 0; i--) {
+				const j = Math.floor(Math.random() * (i + 1));
+				[itemIndices[i], itemIndices[j]] = [itemIndices[j], itemIndices[i]];
+			}
+
+			const shuffledItems = itemIndices.map(index => items.eq(index));
+			$(this).empty().append(firstItem).append(shuffledItems).append(lastItem);
+		});
+	}
+});
