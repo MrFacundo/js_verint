30f26fe22847ed1ab45a525c49209b90c6fdaa31
MrFacundo <ftroitero@gmail.com>
2025-09-24 15:06:07 +0100

    clean all functions

--- list/hideBlocksBasedOnPreviousResponse.js
+++ list/hideBlocksBasedOnPreviousResponse.js
@@ -2,22 +2,22 @@
 $(function () {
     const $originalList = $('.response-set');
     const listItems = $originalList.children('li');
+    const condition1 = "%[4]Q424LBL%" == "Never";
+    const condition2 = "%[4]Q426LBL%" == "Never";
+    const ranges = [
+        { condition: condition1, start: 1, end: 4 },
+        { condition: condition2, start: 5, end: 9 }
+    ];
 
     function hideListItems(ranges) {
-        ranges.forEach(([condition, start, end]) => {
-            if (condition) {
-                for (let i = start; i <= end; i++) {
+        ranges.forEach(function(range) {
+            if (range.condition) {
+                for (let i = range.start; i <= range.end; i++) {
                     listItems.eq(i - 1).hide();
                 }
             }
         });
     }
 
-    const condition1 = "%[4]Q424LBL%" == "Never";
-    const condition2 = "%[4]Q426LBL%" == "Never";
-
-    hideListItems([
-        [condition1, 1, 4],
-        [condition2, 5, 9]
-    ]);
+    hideListItems(ranges);
 });
