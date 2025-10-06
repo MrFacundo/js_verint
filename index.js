09972c4d15038623f6b3980ff7fb0ccffe62e3e1
MrFacundo <ftroitero@gmail.com>
2025-10-06 12:17:11 +0100

    added export

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ export { autoSelect } from "./list/autoSelect.js";
 export { hideEmptyLabelList } from "./list/hideEmptyLabelList.js";
 export { exclusiveOptionPerColumn } from "./matrix/exclusiveOptionPerColumn.js";
 export { maxAnswersPerColumnAllowed } from "./matrix/maxAnswersPerColumnAllowed.js";
+export { addImages } from "./other/addImages2.js";
--- other/addImages2.js
+++ other/addImages2.js
@@ -1,34 +1,18 @@
-// Add source attribute to images
-arrayImg = [
-    '<img src="%[IMG SRC]Q214_1%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_2%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_3%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_4%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_5%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_6%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_7%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_8%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_9%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_10%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_11%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_12%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_13%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_14%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_15%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_16%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_17%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_18%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_19%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_20%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_21%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_22%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_23%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_24%" style="width: auto; height: 80px;">',
-    '<img src="%[IMG SRC]Q214_25%" style="width: auto; height: 80px;">',
-];
-
-$(function () {
-    for (let i = 0; i < arrayImg.length; i++) {
-        $(`#img${i + 1}`).html(arrayImg[i]);
+/**
+ * Insert images with custom styles
+ * @param {Array} imageSources - Array of image source strings
+ * @param {string} idPrefix - Prefix for the jQuery ID selector
+ * @param {Object} styles - Object containing CSS style properties
+ */
+export function addImages(imageSources, idPrefix, styles) {
+    const styleString = Object.keys(styles)
+        .map(function(key) {
+            return key + ': ' + styles[key];
+        })
+        .join('; ');
+    
+    for (let i = 0; i < imageSources.length; i++) {
+        const imgTag = '<img src="' + imageSources[i] + '" style="' + styleString + ';">';
+        $('#' + idPrefix + (i + 1)).html(imgTag);
     }
-});
+}
